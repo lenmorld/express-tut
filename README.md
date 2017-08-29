3c2d12b90719033bef759df7521437cee6ddab28
lenmorld <lenmorld@gmail.com>
2017-08-29 16:01:25 -0400

    app.param()

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ Following code school Express tut
 
 # to print response headers, use -i
 curl -i http://localhost:3001/foods
+
+
+# url params
+curl -i http://localhost:3001/foods?limit=1
+ curl -i http://localhost:3001/foods/banana
--- app.js
+++ app.js
@@ -21,10 +21,127 @@ app.get('/', function(request, response) {
 // });
 
 
+// URL
+// limit number of foods returned
+
+// /foods?limit=2
 app.get('/foods', function(request, response) {
-	var foods = ['banana', 'fries', 'bbq'];
-	response.send(foods);
+    var foods = ['banana', 'fries', 'bbq'];
+    if (request.query.limit >= 0) {
+		response.json(foods.slice(0, request.query.limit))
+	} else {
+        response.send(foods);
+    }
 });
+
+var foods = {
+	'banana': 'yellow and long',
+	'fries': 'Greasy and crispy',
+	'bbq': 'Grilled and spicy'
+};
+
+
+var drinks = {
+	'juice': 'fruity and sugary',
+	'soda': 'bubbly and carbonated',
+	'coffee': 'dark and caffeinated'
+};
+
+
+// JSON object route
+// Object.keys()
+app.get('/foods', function(request, response) {
+    response.send(Object.keys(foods));		// get foods instead of desc
+});
+
+// full obj
+app.get('/foods_complete', function(request, response) {
+    response.send(foods);		// get foods instead of desc
+});
+
+
+// Dynamic Routes (all other are static)
+// foods/banana
+// app.get('/foods/:name', function (request, response) {
+// 	var description = foods[request.params.name];	// undefined if not there
+// 	if (!description) {
+// 		response.status(404)
+// 			.json('No desc found for ' + request.params.name);
+// 	} else {
+//         response.json(description);		// defaults to 200 Success
+//     }
+// });
+
+
+// improvement
+
+// app.get('/foods/:name', function (request, response) {
+//
+// 	// normalize for case-insensitive
+// 	var name = request.params.name;
+// 	var food = name.toLowerCase();
+//     var description = foods[food];	// undefined if not there
+//
+// 	if (!description) {
+// 		response.status(404)
+// 			.json('No desc found for ' + request.params.name);
+// 	} else {
+// 		response.json(description);		// defaults to 200 Success
+// 	}
+// });
+//
+// app.get('/drinks/:name', function (request, response) {
+//
+//     // normalize for case-insensitive
+//     var name = request.params.name;
+//     var drink = name.toLowerCase();
+//     var description = drinks[drink];	// undefined if not there
+//
+//     if (!description) {
+//         response.status(404)
+//             .json('No desc found for ' + request.params.name);
+//     } else {
+//         response.json(description);		// defaults to 200 Success
+//     }
+// });
+
+
+// improvement2
+
+// intercept param then attach ot to request obj which is accessible throughout
+app.param('name', function (request, response, next) {
+    // normalize for case-insensitive
+    var name = request.params.name;
+    var paramName = name.toLowerCase();
+
+    // do other param cleansing here
+	request.paramName = paramName;
+	next();
+});
+
+app.get('/foods/:name', function (request, response) {
+    var description = foods[request.paramName];	// undefined if not there
+	if (!description) {
+		response.status(404)
+			.json('No desc found for ' + request.params.name);
+	} else {
+
+		response.jso
+		response.json(description);		// defaults to 200 Success
+	}
+});
+
+app.get('/drinks/:name', function (request, response) {
+    var description = drinks[request.paramName];	// undefined if not there
+    if (!description) {
+        response.status(404)
+            .json('No desc found for ' + request.params.name);
+    } else {
+        response.json(description);		// defaults to 200 Success
+    }
+});
+
+
 
 app.listen(3001, function() {
 	console.log('Listening at 3001');
